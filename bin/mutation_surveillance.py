bda8c0fed76a8fc28f8030e219ad608238f62ef9
Fuhaoyi <fhy11235813@gmail.com>
2023-01-10 21:40:07 +0800

    add BA.5.24 and BF.7.14

--- bin/mutation_surveillance.py
+++ bin/mutation_surveillance.py
@@ -1,11 +1,10 @@
-import pandas as pd
 
 from utils import *
 
 if __name__ == '__main__':
 
-    data = pd.read_csv('/media/data/fuhaoyi/sequencing_data/20230108/output/6tree/20230108_nextclade.csv',
-                       sep=';')
+    data = pd.read_csv('/media/data/fuhaoyi/sequencing_data/20230108/'
+                       'output/6tree/20230108_nextclade.csv', sep=';')
     data = data.fillna(value='')
     #print(data)
 
@@ -35,12 +34,25 @@ if __name__ == '__main__':
     ref_lineage = pd.read_csv(path / 'BF.7.csv')
     bf7 = different_mutaion(data, ref_lineage)
 
+    path = Path('../src/ref_lineage_mutation')
+    ref_lineage = pd.read_csv(path / 'BF.7.14.csv')
+    bf714 = different_mutaion(data, ref_lineage)
+
+    path = Path('../src/ref_lineage_mutation')
+    ref_lineage = pd.read_csv(path / 'BA.5.2.48.csv')
+    ba5248 = different_mutaion(data, ref_lineage)
+
+
     output_data = pd.DataFrame({
         'seqName': data['seqName'],
         'clade': data['clade'],
         'pango': data['Nextclade_pango'],
+        'diff_BA.5.2.48': ba5248,
+        'diff_BF.7.14': bf714,
         'diff_BA.5.2': ba_5_2,
         'diff_BF.7': bf7,
+        'aaSubstitutions':data['aaSubstitutions']
+
     })
     print(output_data)
     output_data.to_csv('../results/diff_mutations.csv', index=False)
